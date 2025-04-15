b5204015cddf42bccb6bf1196345f657c6957b42
Shadichy <shadichy@blisslabs.org>
2025-04-15 16:55:36 +0700

    testing multiarch
    Signed-off-by: Shadichy <shadichy@blisslabs.org>

--- get.sh
+++ get.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-case "${DEB_BUILD_ARCH:-${DEB_ARCH:-amd64}}" in
+ARCH=${DEB_BUILD_ARCH:-${DEB_ARCH:-${1:-amd64}}}
+
+case "$ARCH" in
 amd64) EFI_ARCH_SHORT=x64 ;;
 i386) EFI_ARCH_SHORT=ia32 ;;
 arm64) EFI_ARCH_SHORT=aa64 ;;
 riscv64) EFI_ARCH_SHORT=riscv64 ;;
 loong64) EFI_ARCH_SHORT=loongarch64 ;;
 *)
-	echo "Unsupported architecture: ${DEB_BUILD_ARCH:-${DEB_ARCH}}"
+	echo "Unsupported architecture: $ARCH"
 	exit 1
 	;;
 esac
